031bda8b5370f5bc17a326cbcb395523bb5041e2
Luis Mayta <luis.mayta@gmail.com>
2017-11-29 00:06:05 -0500

    Servicios

--- resources/views/web/capacitaciones.blade.php
+++ resources/views/web/capacitaciones.blade.php
@@ -6,7 +6,7 @@
 	<div class="container">
 		<div class="row align-items-center">
 			<div class="col-lg-6">
-				<h1>- Novedades <span>Tenemos las siguientes novedades</span></h1>
+				<h1>- Capacitaciones <span></span></h1>
 			</div>
 			<div class="col-lg-6">
 				<ul class="breadcrumb pull-right">
@@ -18,4 +18,18 @@
 		</div>
 	</div>
 </section>
+<div class="container">
+	<div class="row pt-1 pb-4 mb-3">
+		<div class="col">
+			<p class="lead mb-4">IN HOUSE</p>
+			<img src="{{ asset('img/demos/business-consulting/cases/case-5.jpg') }}" class="img-fluid mb-4" alt="" align="right">
+			<p>Previo diagnóstico preparamos la capacitación de acuerdo a tus necesidades.
+			Lograremos la competencia de tu personal.</p>
+			<p class="lead mb-4">DISPONIBLE A PÚBLICO EN GENERAL</p>
+			<p>Nuestros capacitadores  especialista desarrollan cursos de mayor demanda en Calidad, Seguridad, Ambiente, Contabilidad, Turismo, Derecho, Administración, Educación, entre otros.
+			<p>En las capacitaciones encontraras respuestas a tus necesidades.</p>
+			<p>Aplicamos una metodología personalizada</p>
+		</div>
+	</div>
+</div>
 @endsection
