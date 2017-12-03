15bc976bb541532fd9d486157fff10c9f228284c
Luis Mayta <luis.mayta@gmail.com>
2017-12-03 17:30:40 -0500

    mapa

--- resources/views/web/contactanos.blade.php
+++ resources/views/web/contactanos.blade.php
@@ -75,8 +75,8 @@
 						<div class="feature-box-info">
 							<h2 class="font-weight-bold text-color-dark">- Dirección</h2>
 							<p class="text-4">
-								123 Porto Blvd, Suite 100<br>
-								New York, NY
+								Jiron Teodoro Cardenas N° 475 Int. 202  <br>
+								Santa Beatriz Lima
 							</p>
 						</div>
 					</div>
@@ -118,4 +118,5 @@
 
 	</div>
 </div>
+<div id="googlemaps" class="google-map mt-4 mb-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.510937511219!2d-77.03404587089273!3d-12.077133695003496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c88cd6055a9b%3A0x478434ba740df93e!2sTeodoro+C%C3%A1rdenas+475%2C+Cercado+de+Lima+15046!5e0!3m2!1ses!2spe!4v1512340032133" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
 @endsection
